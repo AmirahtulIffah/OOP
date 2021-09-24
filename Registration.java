d6776ce284cd5470ba030cdf48fbdaadf50a0dad
AmirahtulIffah <83347360+AmirahtulIffah@users.noreply.github.com>
2021-09-24 21:42:52 +0800

    Update Registration.java

--- Registration.java
+++ Registration.java
@@ -5,17 +5,24 @@ public class Registration {
 	private int id;
 	private String username;
 	private String password;
+	private int cardNumber;
+	private double cardBalance;
+	
 	
 	Registration(){
 		id = 0;
 		username = "";
 		password = "";
+		cardNumber= 0;
+		cardBalance= 0;
 	}
 	
-	public Registration (int newId, String newUsername, String newPassword) {
+	public Registration (int newId, String newUsername, String newPassword, int newCardNumber, int newCardBalance) {
 		id = newId;
 		username = newUsername;
 		password = newPassword;
+		cardNumber = newCardNumber;
+		cardBalance = newCardBalance;
 	}
 	
 	public void setId(int newId) {
@@ -28,6 +35,12 @@ public class Registration {
 	
 	public void setPassword(String newPassword) {
 		password = newPassword;
+	}
+	public void setCardNumber(int newCardNumber) {
+		cardNumber= newCardNumber;
+	}
+	public void setBalance(double newCardBalance) {
+		cardBalance = newCardBalance;
 	}
 	
 	public int getId() {
@@ -42,7 +55,16 @@ public class Registration {
 		return password;
 	}
 	
+	public int getCardNumber() {
+		return cardNumber;
+	}
+	
+	public double getCardBalance() {
+		return cardBalance;
+	}
+	
 	public String toString() {
-		return "Account registered" + "\n" + "ID:" + getId() + "\n" + "Username:" + getUsername() + "\n" + "Password:" + getPassword();
+		return "Account registered" + "\n" + "ID:" + getId() + "\n" + "Username:" + getUsername() + "\n" + "Password:" + getPassword()
+		+ "\n" + "Card Number:" + getCardNumber()+ "\n" + "Card Balance:" + getCardBalance();
 	}
 }
